14fb26ab70ab686ea86884eee365ef0ac65cac7f
Toby Jackson <toby@tobyjackson.net>
2014-02-01 12:59:56 +0000

    Opened firewall for ports 80, 2003,2004 and 7002 (http, and carbon)

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,4 +5,11 @@ include epel
 node "graphite.tobyjackson.net" {
   notice("Configuring node 'graphite'")
   include graphite
+  
+  iptables::allow { 'tcp/80': port => '80', protocol => 'tcp' }
+  # Carbon ports - newline, delimited and pickle protocols
+  iptables::allow { 'tcp/2003': port => '2003', protocol => 'tcp' }
+  iptables::allow { 'tcp/2004': port => '2004', protocol => 'tcp' }
+  iptables::allow { 'tcp/7002': port => '7002', protocol => 'tcp' }
+  
 }
